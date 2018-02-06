8b2c04bfffbcffc907790ddb9b827f51d2ad521f
BIT Student <classroom@bgit.rs>
2018-02-06 17:53:08 +0100

    Functions

---
+++ functions/task1.js
@@ -0,0 +1,7 @@
+function max(a, b) {
+    var c = a + b;
+    return console.log(c);
+
+}
+
+max(7,8);
---
+++ functions/task2.js
@@ -0,0 +1,9 @@
+function odd(a) {
+    if (a%2!==0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(odd(6));
