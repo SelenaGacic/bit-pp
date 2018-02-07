5869fcfe30f4f0e749b897a098ea6803db1bfec4
BIT Student <classroom@bgit.rs>
2018-02-07 16:38:01 +0100

    Functions

--- functions/task11.js
+++ functions/task11.js
@@ -1,6 +1,12 @@
 var string = "asd";
+var sum = "";
 
 function con (str, num) {
-    var duzinaNovogStringa = num * str.length;
-    
+    for (var i=0; i < str.length; i++) {
+        sum += str;
+        
+    }
+    return sum;
 }
+
+console.log(con(string,3));
